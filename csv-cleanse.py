2d5a3010752ea5ff1cafc32d9e80b90c4c2c5336
Sean <sean.tesch@gmail.com>
2019-03-12 09:35:28 -0400

    Cleansed the countires csv
    Used python to cleanse the white space from the region
    column in the countries csv.
    
    (Use countries.csv for cleansed data)

--- csv-cleanse.py
+++ csv-cleanse.py
@@ -2,7 +2,7 @@
 import re
 
 def main():
-    cleanse()
+    clean_world()
 
 def cleanse():
     out = open("athletes.csv", "w")
@@ -15,6 +15,17 @@ def cleanse():
             num = str(num).replace('\']', '')
             line = re.sub('\s\d+,\d+\s', str(num), line)
             out.write(line)
+    out.close()
+
+def clean_world():
+    out = open("countries.csv", "w")
+    with open("countries_data.csv", "r") as file:
+        for line in file:
+            line = line.split(',')
+            line[0] = line[0].strip()
+            line[1] = line[1].strip()
+            myList = ','.join(map(str, line)) 
+            out.write(myList)
     out.close()
 
 if __name__ == "__main__":
